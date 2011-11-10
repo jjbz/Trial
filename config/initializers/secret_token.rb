0f7b8aace30a6027c82d09bf0bab4833c6546f4b
John Beahan <jjb@zestcash.com>
2011-11-10 00:16:28 -0800

    setting up new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trial::Application.config.secret_token = '73209ecce6828611c086bf8950962bb5c9834378f215ea9f1b099323fa4222e91a776066b75ba87fa7ff06ef6e926e0873b6efcf6304b64b9498e839266d4999'
